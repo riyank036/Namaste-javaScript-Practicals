e4bfb81eef366f11743f0328d471139a830d2691
unknown <riyankpatel036@gmail.com>
2025-04-22 02:02:36 +0530

    Hoisting in JavaScript (variables & functions)

---
+++ E-03/script.js
@@ -0,0 +1,25 @@
+// Normal 
+
+// var x = 7;
+// function getName() {
+//     console.log("Hello World!");
+// }
+
+// getName();
+// console.log(x);
+
+
+
+// Hoisting 
+
+getName(); //Hello World!
+console.log(x); //undefined
+
+var x = 7;
+
+console.log(x); // 7
+
+
+function getName () {
+    console.log("Hello World!") 
+}
