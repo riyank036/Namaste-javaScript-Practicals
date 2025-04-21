7e679f70da783b77daebeaeb563eb1d69bd50276
unknown <riyankpatel036@gmail.com>
2025-04-22 01:54:31 +0530

    How JS is executed & Call Stack

---
+++ E-02/script.js
@@ -0,0 +1,10 @@
+var n = 2;
+function squere(num) {
+    var ans = num * num;
+    return ans;
+}
+var squere2 = squere(n); 
+var squere4 = squere(4);
+
+console.log(squere2); // 4
+console.log(squere4); // 16
